1789d2f4ae426cdbf5829a26e2814cedeed1e72b
KuthikBohdan1 <bkuzik14@gmail.com>
2025-05-30 21:37:52 +0300

    3

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from app.models import Products
 
 
-def product_list(request):
+def products_list(request):
     products = Products.objects.all()
     context = {
         "products_list": products,
@@ -12,3 +12,8 @@ def product_list(request):
         template_name="myapp/products_list.html",
         context=context,  
     )
+
+
+def products_detail(request):
+    # Логіка для відображення деталей продукту
+    pass
